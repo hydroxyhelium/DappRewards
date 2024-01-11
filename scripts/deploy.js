b75e0148048516cfb7a25894878a3596e9d08743
Priyanshu Sharma <sharmapriyanshu119@gmail.com>
2024-01-11 18:11:53 +0530

    Added the deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,3 +2,16 @@ const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
+async function main() {
+    const GiveAway = await ethers.getContractFactory("GiveAway");
+    // Start deployment, returning a promise that resolves to a contract object
+    const give_away = await HelloWorld.deploy();   
+    console.log("Contract deployed to address:", give_away.address);
+}
+ 
+main()
+.then(() => process.exit(0))
+.catch(error => {
+    console.error(error);
+    process.exit(1);
+});
